c1af492e069622f2292f27562a9bc5ac8d7ff403
hhsdev <heinhtetsoe.dev@gmail.com>
2020-02-14 02:47:57 +0630

    Remove code that draws stuff from Drawing

---
+++ src/drawing.js
@@ -0,0 +1,13 @@
+"use strict";
+
+class Drawing {
+	constructor(width, height) {
+		this.elementTagStart = '<svg width="' + width + '" height="' + height + '" xmlns="http://www.w3.org/2000/svg">';
+		this.elementTagEnd = '</svg>';
+		this.html = '';
+	}
+
+	attachTo(container) {
+		container.innerHTML = this.elementTagStart + this.html + this.elementTagEnd;
+	}
+}
