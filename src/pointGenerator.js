97488ceb36ec524a87e44af0e500fbfb038fa65a
hhsdev <heinhtetsoe.dev@gmail.com>
2020-02-14 08:41:04 +0630

    Add PointGenerator class

---
+++ src/pointGenerator.js
@@ -0,0 +1,47 @@
+"use strict";
+
+class PointGenerator {
+  constructor(x0, y0, x1, y1) {
+    this.x0 = x0;
+    this.y0 = y0;
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x = null;
+    this.y = null;
+  }
+
+  xIs(x) {
+    this.x = x;
+    return this;
+  }
+
+  yIs(y) {
+    this.y = y;
+    return this;
+  }
+
+  distanceFromTopBorder(distance) {
+    this.y = distance;
+    return this;
+  }
+
+  distanceFromBottomBorder(distance) {
+    this.y = this.y1 - distance;
+    return this;
+  }
+
+  distanceFromRightBorder(distance) {
+    this.x = this.x1 - distance;
+    return this;
+  }
+
+  distanceFromLeftBorder(distance) {
+    this.x = distance;
+    return this;
+  }
+
+  generate() {
+    const { x, y } = this;
+    return {x, y};
+  }
+}
