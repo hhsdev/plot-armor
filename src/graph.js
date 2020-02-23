43d3efc45a37c17c992389c26371b09b2853edee
hhsdev <heinhtetsoe.dev@gmail.com>
2020-02-23 06:49:46 +0630

    Refactor src/graph.js

--- src/graph.js
+++ src/graph.js
@@ -1,18 +1,62 @@
 "use strict";
 
 class Graph {
-  constructor(height, width, padding, drawing, yAxis, xAxis) {
-    this.height = height;
-    this.width = width;
-    this.padding = padding;
-    this.drawing = drawing;
+  constructor(config) {
+    this.width = config.width || 600;
+    this.height = config.height || 600;
+    this.padding = config.padding || 30;
+    this.drawing = config.drawing || new Drawing(this.width, this.height);
 
-    this.yAxis = yAxis;
-    this.xAxis = xAxis;
+    this.xAxis = this.createXAxis(config);
+    this.yAxis = this.createYAxis(config);
 
-    this.pointGenerator = new PointGenerator(0, 0, width, height);
+    this.pointGenerator = new PointGenerator(0, 0, this.width, this.height);
     this.pen = new LinePen().setThickness(1).setLineColor("black");
     this.plotLines = [];
+  }
+
+  createXAxis(config) {
+    let { xAxis } = config;
+    if (!xAxis) {
+      xAxis = {
+        xOffset: 30,
+        yOffset: 30,
+        fontSize: 16,
+        ticks: 28,
+        minorTicksPerMajorTick: 4,
+        majorTickSize: 10,
+        minorTickSize: 5
+      };
+    }
+    return new Axis(
+      "horizontal",
+      "X-Axis",
+      this.drawing,
+      this.width,
+      this.height,
+      this.padding,
+      xAxis.fontSize || 16,
+      xAxis.ticks || 28,
+      xAxis.minorTicksPerMajorTick || 4,
+      xAxis.majorTickSize || 10,
+      xAxis.minorTickSize || 5
+    );
+  }
+
+  createYAxis() {
+    return new Axis(
+      "vertical",
+      "Y-Axis",
+      this.drawing,
+      600,
+      600,
+      30,
+      16,
+      28,
+      4,
+      10,
+      5
+    );
   }
 
   attachTo(container) {
