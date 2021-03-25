abf0f53a4bb736caedda09e5bd59e3a0d8612f3c
hhsdev <heinhtetsoe.dev@gmail.com>
2021-03-25 14:18:33 +0630

    Modify rollup.config.js to output a minified version as well

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,14 @@
+import { terser } from "rollup-plugin-terser";
+
 module.exports = {
   input: ["src/main.js"],
-  output: {
-    file: "bundle.js",
-    name: "Graph"
-  }
+  output: [
+    { file: "bundle.js", format: "iife", name: "Graph" },
+    {
+      file: "bundle.min.js",
+      name: "Graph",
+      format: "iife",
+      plugins: [terser()],
+    },
+  ],
 };
